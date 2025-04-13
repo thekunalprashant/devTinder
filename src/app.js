26b11f9d121f03d9bb4d7abe1debbd528e090c25
Kunal Prashant <kunalprashant77@gmail.com>
2025-04-14 03:53:25 +0530

    created a server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.use("/",(req, res)=>{
-    res.send("namaste kunal!")
-})
+app.use("/", (req, res) => {
+  res.send("namaste kunal!");
+});
 
 app.use("/test", (req, res) => {
   res.send("Server is live...");
+});
+app.use("/test/2", (req, res) => {
+  res.send("Testing...");
 });
 
 app.listen(7777, () => {
