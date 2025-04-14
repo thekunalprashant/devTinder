7f39466e43ef550222fb1dc691d2eeafa5ebcf6f
Kunal Prashant <kunalprashant77@gmail.com>
2025-04-14 22:55:39 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("namaste kunal!");
+app.get(/.*fly$/, (req, res) => {
+  res.send("jsjjssjs");
 });
-
-app.use("/test", (req, res) => {
-  res.send("Server is live...");
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  const queryP = req.query;
+  res.send({ firstName: "Kunal", lastName: "Prashant" , queryP});
 });
-app.use("/test/2", (req, res) => {
-  res.send("Testing...");
+app.post("/user", (req, res) => {
+  res.send("User Data Saved to database.");
 });
+app.delete("/user", (req, res) => {
+  res.send("User deleted successfully");
+});
+// app.use("/", (req, res) => {
+//   res.send("namaste kunal!");
+// });
 
 app.listen(7777, () => {
   console.log("Server is running....");
