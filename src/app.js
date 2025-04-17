a7cfc10261a6614e2a6f5d93f4e3f455268a80e5
Kunal Prashant <kunalprashant77@gmail.com>
2025-04-17 18:40:09 +0530

    data sanitization and schema validations

--- src/app.js
+++ src/app.js
@@ -1,19 +1,80 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middlewares/auth");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length == 0) {
+      res.status(404).send("User Not found!");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong!");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const allowedUpdates = [
+      "firstName",
+      "lastName",
+      "about",
+      "skills",
+      "password",
+      "age",
+      "gender",
+      "photoUrl",
+    ];
+    const isAllowedUpdates = Object.keys(data).every((item) =>
+      allowedUpdates.includes(item)
+    );
+    if (!isAllowedUpdates) {
+      throw new Error("Updates not allowed, due to invalid data submission");
+    }
+    if (data?.skills?.length > 10) {
+      throw new Error("Skills can not be more than 10");
+    }
+    const updatedUser = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(updatedUser);
+    res.send("User Updated!" + updatedUser);
+  } catch (error) {
+    res.status(400).send("Error Failed: " + error.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Kunal",
-    lastName: "Prashant",
-    emailId: "kunalkabir7@gmail.com",
-    password: "Kunal@112",
-    age: 24,
-    gender: "Male",
-  };
+  const userObj = req.body;
   const user = new User(userObj);
   try {
     await user.save();
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,72 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password " + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("geneder is not valid!");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fillustrations%2Favatar-icon-placeholder-symbol-659652%2F&psig=AOvVaw1SvGiwwxR8T8C0OaelHDoW&ust=1744972044912000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjO7Mjt3owDFQAAAAAdAAAAABAE",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Photo Url is not valid");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "Hey there, this is my about!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 const User = mongoose.model("User", userSchema);
 module.exports = User;
